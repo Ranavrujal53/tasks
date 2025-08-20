17a814313e5cbed01a754bbd12c4116426913539
Ranavrujal53 <csrana1213@gmail.com>
2025-08-20 21:37:01 +0530

    Add files via upload

---
+++ prectice/name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+    printf("******************************\n");
+    printf("*                            *\n");
+    printf("*         RANA VRUJAL        *\n");
+    printf("*                            *\n");
+    printf("******************************\n");
+
+    return 0;
+}
+
---
+++ prectice/profile.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    printf("-------------------------------------\n");
+    printf("\t   PROFILE PAGE              \n");
+    printf("-------------------------------------\n");
+    printf("Name:\tRana Vrujal\n");
+    printf("Email:\tcsrana1213@gmail.com\n");
+    printf("Number:\t+91 9825560337\n");
+    printf("Address:\tKosmaba\n");
+    printf("-------------------------------------\n");
+    
+    return 0;
+}
